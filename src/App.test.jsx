2a127c504e91cf477213286d53fbb4f9b90e03b4
thkim <thkim2017@gmail.com>
2021-03-26 12:52:02 +0900

    테스트를 적용하라 jest를 세팅하고 테스트를 시작했습니다. 항상 import하고 싶은 '@testing-library/jest-dom'을 conifg에 적용 시도했습니다. 하지만 jest.config.js와 jest.setup.js가 적용되지 않는 문제가 있습니다. 따라서 일단 App.test.jsx 파일에 직접 적용했습니다.

---
+++ src/App.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import '@testing-library/jest-dom';
+
+import App from './App';
+
+test('App', () => {
+  const { container } = render((
+      <App />
+    ));
+
+  expect(container).toHaveTextContent(/운동 기록장/);
+});
